ac5d4efebb8b627fbf992abcd9fbf19f75e75945
DavidBudnitskyBBD <davidbu@bbd.co.za>
2024-07-23 09:38:03 +0200

    Added API Setup

---
+++ Backend/API/Services/IPreferenceService.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Services
+{
+	public interface IPreferenceService
+	{
+		IEnumerable<Preference> GetPreferences();
+		Preference SetPreferences(Preference newPreference);
+	}
+}
