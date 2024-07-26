7b507f5c35540a3c10c3a13fa68f0476cadff647
jennadunford <jenna.dunford@bbd.co.za>
2024-07-26 06:14:16 +0200

    added sm fonts

--- Frontend/src/scripts/script.js
+++ Frontend/src/scripts/script.js
@@ -199,7 +199,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   // checkLoggedIn();
 
-  const fonts = ["Arial", "Times New Roman", "Verdana", "Georgia", "Roboto","Pacifico"]; //Get from backend?
+  const fonts = ["Arial", "Times New Roman", "Verdana", "Georgia", "Roboto","Pacifico","Anton","Orbitron","Rowdies"]; //Get from backend?
 
   document.body.style.fontSize = defaultFontSize;
   document.body.style.color = defaultColor;
